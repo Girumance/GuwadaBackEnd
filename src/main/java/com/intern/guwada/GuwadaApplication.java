18d8d3f331a26b48051f8b08fe3621c33009aa5c
Girum Kedese <girumkedese@gmail.com>
2019-09-20 13:10:54 -0400

    guwada project setup

---
+++ src/main/java/com/intern/guwada/GuwadaApplication.java
@@ -0,0 +1,13 @@
+package com.intern.guwada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuwadaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuwadaApplication.class, args);
+    }
+
+}
